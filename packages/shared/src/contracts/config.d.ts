7f17fe39972308f13e2fa15cf5d191566376d7ce
svengrav <sven@svengrav.de>
2026-01-13 14:51:29 +0000

    removed js files

--- packages/shared/src/contracts/config.d.ts
+++
@@ -1,8 +0,0 @@
-export interface FirebaseConfig {
-    apiKey: string;
-    appId: string;
-    projectId: string;
-    messagingSenderId: string;
-    storageBucket: string;
-    databaseURL: string;
-}
--- packages/shared/src/events/eventHandler.d.ts
+++
@@ -1,7 +0,0 @@
-export type EventCallback<Payload> = (payload: Payload) => void;
-export type Unsubscribe = () => void;
-export declare const createEventSystem: <Events extends Record<string, any>>() => {
-    on<K extends keyof Events>(event: K, callback: EventCallback<Events[K]>): Unsubscribe;
-    off<K extends keyof Events>(event: K, callback: EventCallback<Events[K]>): void;
-    emit<K extends keyof Events>(event: K, payload: Events[K]): void;
-};
